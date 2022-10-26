67158936429a9b21453f72c0380ca15198974977
Giovanni Silveira Brasil <61767099+brasilgiovanni@users.noreply.github.com>
2022-10-26 12:41:18 +0000

    exercises about graphics

---
+++ Exercise06_graphics.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Implementar uma solução em Python para gerar 1000 pontos seguindo a distribuição normal com:
+# Média = 20
+# Desvio Padrão = 2
+# Número de pontos = 1000
+# Exibir os dados em um histograma
+
+# C Ó D I G O 
+# np.random.seed(1)
+# dados = np.random.normal(loc = 20, scale = 2, size = 1000)
+# plt.hist(dados, color = 'lightblue', ec = 'red')
+# plt.show()
+
+# Implementar uma solução em Python para visualizar dados de venda de produtos em um gráfico de barras.
+
+# C Ó D I G O 
+# produtos = ['A', 'B', 'C', 'D']
+# vendas = [10, 4, 6, 2]
+# x = np.array(produtos)
+# y = np.array(vendas)
+# plt.bar(x, y)
+# plt.show()
+
